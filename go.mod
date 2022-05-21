52c5474237674c90ddaefc8a7c2d408e0b7964f1
Igor Guedes <igorguedes@MacBook-Pro-de-Igor.local>
2022-05-20 23:02:20 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,27 @@
+module lulu
+
+go 1.18
+
+require (
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/charmbracelet/bubbles v0.10.3 // indirect
+	github.com/charmbracelet/bubbletea v0.20.0 // indirect
+	github.com/charmbracelet/lipgloss v0.4.0 // indirect
+	github.com/containerd/console v1.0.3 // indirect
+	github.com/gdamore/encoding v0.0.0-20151215212835-b23993cbb635 // indirect
+	github.com/gdamore/tcell v1.1.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/marcusolsson/tui-go v0.4.0 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.0 // indirect
+	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.11.1-0.20220212125758-44cd13922739 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sahilm/fuzzy v0.1.0 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/term v0.0.0-20210422114643-f5beecf764ed // indirect
+	golang.org/x/text v0.3.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20220512140231-539c8e751b99 // indirect
+)
---
+++ main.go
@@ -0,0 +1,134 @@
+package main
+
+// A simple example that shows how to retrieve a value from a Bubble Tea
+// program after the Bubble Tea has exited.
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"gopkg.in/yaml.v3"
+)
+
+var choices = []string{}
+
+type Config struct {
+	Name           string `yaml:"name,omitempty"`
+	ConfigFileName string `yaml:"config-file-name,omitempty"`
+}
+
+type Configs struct {
+	Configs []*Config `yaml:"kube-config,omitempty"`
+}
+type model struct {
+	cursor  int
+	choice  string
+	actions []*Config
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q", "esc":
+			return m, tea.Quit
+
+		case "enter":
+			// Send the choice on the channel and exit.
+			m.choice = choices[m.cursor]
+			return m, tea.Quit
+
+		case "down", "j":
+			m.cursor++
+			if m.cursor >= len(choices) {
+				m.cursor = 0
+			}
+
+		case "up", "k":
+			m.cursor--
+			if m.cursor < 0 {
+				m.cursor = len(choices) - 1
+			}
+		}
+
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	s := strings.Builder{}
+	s.WriteString("Bem vindo ao lulu cluster handler! escolha uma config\n\n")
+
+	for i := 0; i < len(choices); i++ {
+		if m.cursor == i {
+			s.WriteString("(🤏) ")
+		} else {
+			s.WriteString("( ) ")
+		}
+		s.WriteString(choices[i])
+		s.WriteString("\n")
+	}
+	s.WriteString("\n(press q to quit)\n")
+
+	return s.String()
+}
+
+func main() {
+	yfile, err := ioutil.ReadFile("config/config.yml")
+
+	if err != nil {
+
+		log.Fatal(err)
+	}
+
+	configs := &Configs{}
+	err2 := yaml.Unmarshal(yfile, &configs)
+
+	if err2 != nil {
+
+		log.Fatal(err2)
+	}
+
+	for _, v := range configs.Configs {
+		choices = append(choices, v.Name)
+	}
+
+	mm := model{}
+	mm.actions = configs.Configs
+	p := tea.NewProgram(mm)
+
+	// StartReturningModel returns the model as a tea.Model.
+	m, err := p.StartReturningModel()
+	if err != nil {
+		fmt.Println("Oh no:", err)
+		os.Exit(1)
+	}
+
+	// Assert the final tea.Model to our local model and print the choice.
+	if m, ok := m.(model); ok && m.choice != "" {
+		for _, v := range m.actions {
+			if v.Name == m.choice {
+				cmd := exec.Command("export", "$KUBECONFIG=~/.kube/"+v.ConfigFileName)
+				stdout, err := cmd.Output()
+
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+
+				// Print the output
+				fmt.Println(string(stdout))
+			}
+		}
+	}
+}
